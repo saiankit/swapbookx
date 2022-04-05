00877d1b4146a590cac637c979fc6b8953cfeb85
Sai Ankit <saiankit30@gmail.com>
2022-04-05 13:38:18 +0530

    🚀 initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+import webpack from 'webpack';
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      //...
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
