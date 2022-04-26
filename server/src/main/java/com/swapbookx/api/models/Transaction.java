9d6b5e6412619c7e10d54a0fe081998839fa9eb7
PavanShyamendra <n.p.shyamendra3322@gmail.com>
2022-04-26 12:16:58 +0530

    Add Transaction.java

---
+++ server/src/main/java/com/swapbookx/api/models/Transaction.java
@@ -0,0 +1,42 @@
+package com.swapbookx.api.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="usersss")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int txId;
+
+    @Column(name="Lender_ID", nullable = false)
+    private int lenderId;
+
+    @Column(name="Borrower_ID", nullable = false)
+    private int borrowerId;
+
+    @Column(name="Book_ID", nullable = false)
+    private int book_Id;
+
+    @Column(name="DateIssued", nullable = false)
+    private Date dateIssued;
+
+    @Column(name="returnDate", nullable = false)
+    private Date returnDate;
+
+}
---
+++ server/src/main/java/com/swapbookx/api/services/impl/TransactionServiceImpl.java
@@ -0,0 +1,7 @@
+package com.swapbookx.api.services.impl;
+
+import com.swapbookx.api.services.TransactionService;
+
+public class TransactionServiceImpl implements TransactionService {
+    
+}
