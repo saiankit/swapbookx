4719c16e8856bacfd3e8b6839dcc4951f6e8a2a1
PavanShyamendra <n.p.shyamendra3322@gmail.com>
2022-04-27 12:04:45 +0530

    Added Update Delete Get of user

--- server/src/main/java/com/swapbookx/api/controllers/UserController.java
+++ server/src/main/java/com/swapbookx/api/controllers/UserController.java
@@ -1,13 +1,22 @@
 package com.swapbookx.api.controllers;
 
+import java.net.http.HttpResponse.ResponseInfo;
+import java.util.List;
+import java.util.Map;
+
+import com.swapbookx.api.models.User;
+import com.swapbookx.api.payloads.ApiResponse;
 import com.swapbookx.api.payloads.UserDto;
 import com.swapbookx.api.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +35,32 @@ public class UserController {
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
     }
 
+    // PUT
+
+    @PutMapping("/{userID}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userID") Integer uid)
+    {
+        UserDto updateUser = this.userService.updateUser(userDto, uid);
+        return ResponseEntity.ok(updateUser);
+    }
+
+    //Delete
+    @DeleteMapping("/{userID}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userID") Integer uid){
+        this.userService.deleteUser(uid);
+        return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted", true),HttpStatus.OK);
+    }
+
+    //Get
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        return ResponseEntity.ok(this.userService.getAllusers());
+    }
+
+    //Get - user
+
+    @GetMapping("/{userID}")
+    public ResponseEntity<UserDto> getSingleUser(@PathVariable("userID") Integer uid){
+        return ResponseEntity.ok(this.userService.getUserById(uid));
+    }
 }
---
+++ server/src/main/java/com/swapbookx/api/payloads/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.swapbookx.api.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ApiResponse {
+    
+    private String message;
+    private boolean success;
+
+}
