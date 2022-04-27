82de7f35fa3fcf1462e237948fc6922d5174e2aa
Sai Ankit <saiankit30@gmail.com>
2022-04-27 22:42:30 +0530

    remove useless shit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './App';
-import 'flowbite'
-
 import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+
 
 ReactDOM.render(
   <BrowserRouter>
